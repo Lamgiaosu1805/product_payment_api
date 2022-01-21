624c7c948f79869651a1654910a7b13f8a4dedef
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-21 11:29:57 +0700

    install nodemon

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('HÊLO LÂM GS!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
