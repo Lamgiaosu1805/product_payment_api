7f6656f2a9db98a5b2a559a4787b4a2567235c58
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-21 11:36:50 +0700

    install morgan package

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
+
+
 const app = express()
 const port = 3000
+
+
+//use Middlewares
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('HÊLO LÂM GS!')
