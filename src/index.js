2c97bd7f920311de828c53d444f3383c626e0a38
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-21 11:54:23 +0700

    cau truc project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 
-
 const app = express()
 const port = 3000
 
+//config static file
+// app.use(express.static(path.join))
 
 //use Middlewares
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.send('HÊLO LÂM GS!')
+  // res.send('HÊLO LÂM GS!')
 })
 
 app.listen(port, () => {
