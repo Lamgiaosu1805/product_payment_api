ce44e94b8d60fb83ed2a405683b08a1fe754cf7d
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-21 19:29:33 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 const route = require('./routes');
 const db = require('./config/db');
