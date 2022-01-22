86d83337b34bb31679ffa746bb92f14116e127c2
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-22 16:12:14 +0700

    connect mySQL online by Sequelize

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
-
+const { Sequelize } = require('sequelize-v5');
 
 async function connect() {
+    // try {
+    //     await mongoose.connect('mongodb+srv://kylanx11:nghiemlamhust1@cluster0.qjbm1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    //     console.log("Connect successfully");
+    // } catch (error) {
+    //     console.log("Connect failure !!");
+    // }
+    const sequelize = new Sequelize('sql6467268', 'sql6467268', 'lamlovequynh1', {
+        host: 'sql6.freesqldatabase.com',
+        dialect: 'mysql'
+    })
     try {
-        await mongoose.connect('mongodb+srv://kylanx11:nghiemlamhust1@cluster0.qjbm1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+        await sequelize.authenticate();
         console.log("Connect successfully");
     } catch (error) {
-        console.log("Connect failure !!");
+        console.log("Connect failure");
     }
 }
 
