5afaf50711d7915db48b5cb5159d12b019fcb11e
Lamgiaosu1805 <91446446+Lamgiaosu1805@users.noreply.github.com>
2022-01-27 12:40:45 +0700

    fix deploy

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -11,13 +11,6 @@ const sequelize = new Sequelize('sql6467268', 'sql6467268', 'lamlovequynh1', {
 });
 
 async function connect() {
-    // try {
-    //     await mongoose.connect('mongodb+srv://kylanx11:nghiemlamhust1@cluster0.qjbm1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-    //     console.log("Connect successfully");
-    // } catch (error) {
-    //     console.log("Connect failure !!");
-    // }
-
     try {
         await sequelize.authenticate();
         console.log("Connect successfully");
